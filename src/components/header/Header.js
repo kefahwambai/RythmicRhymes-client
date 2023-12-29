c5c05e0585878beb61e0cae9db793cdd54f5242d
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-12-29 14:51:46 +0300

    added styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">Charlotte's</span>
-        <span className="headerTitleLg">BLOG</span>
+        <span className="headerTitleSm">Rythmic</span>
+        <span className="headerTitleLg">Ryhmes</span>
       </div>
       <img
         className="headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src="https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
       />
     </div>
